35e3a540b1a188b61c12eeed10ba19156493a92b
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-19 13:39:58 -0300

    FEAT: REQUISITO 12 DONE

---
+++ challenges/desafio12..js
@@ -0,0 +1,6 @@
+db.produtos.updateMany({ nome: 
+    { $ne: "Mcchicken" },
+  },
+    { $addToSet: { ingredientes: "ketchup" },
+  });
+  db.produtos.find({}, { nome: 1, _id: 0, ingredientes: 1 }); 
