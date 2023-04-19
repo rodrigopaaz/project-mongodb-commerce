f1b2dda2c7eaac204d3df31ced6fe4e811d83f14
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-19 19:07:00 -0300

    feat: req 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,8 @@
+use("projeto");
+
 db.produtos.find(
   { ingredientes: {
     $eq: "picles",
   } },
-  { nome: 1, _id: 0, ingredientes: 1 },
+  { nome: 1, _id: 0, ingredientes: 1, valoresNutricionais: { $slice: 3 } },
   );
