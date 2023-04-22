04cd8361066ef3373a71c5e38cb93429938b2b6a
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-22 19:21:06 -0300

    FEAT: FIX REQ 32 ERROR

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,4 +1,5 @@
 db.produtos.updateOne({ nome: "Big Mac" }, {
     $unset: { curtidas: "" },
 });
-db.produtos.find({ nome: { $ne: "Big Mac" } }, { nome: 1, _id: 0, curtidas: 1 });
+db.produtos.find({ nome: { $ne: "Big Mac" } }, 
+{ nome: 1, _id: 0, curtidas: 1 });
