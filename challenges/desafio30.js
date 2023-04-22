7f1bec89f4cdedf9315f5606d95e0a78a8b21d3b
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-22 18:51:38 -0300

    FIX ERRORS 2

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,4 +1,4 @@
 db.produtos.updateOne({ nome: "Big Mac" }, {
     $unset: { curtidas: "" },
 });
-db.produtos.find({ nome: { $ne: "Big Mac" } }, { nome: 1, _id: 0, descricaoSite: 1 });
+db.produtos.find({ nome: { $ne: "Big Mac" } }, { nome: 1, _id: 0, curtidas: 1 });
